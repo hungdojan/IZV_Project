49524d4b6b405cfc509cb6db6365f5105af522ce
Hung Do <hungdojan@gmail.com>
2022-10-24 19:39:12 +0200

    Initial setup

--- Part1/part01.py
+++ Part1/part01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 IZV cast1 projektu
-Autor: 
+Autor: Hung Do
 
 Detailni zadani projektu je v samostatnem projektu e-learningu.
 Nezapomente na to, ze python soubory maji dane formatovani.
@@ -17,8 +17,8 @@ import matplotlib.pyplot as plt
 from typing import List
 
 
-def integrate(x: np.array, y: np.array) -> float:
-    pass
+def integrate(x: np.ndarray, y: np.ndarray) -> float:
+    return 0.0
 
 
 def generate_graph(a: List[float], show_figure: bool = False, save_path: str | None=None):
@@ -34,4 +34,4 @@ def download_data(url="https://ehw.fit.vutbr.cz/izv/temp.html"):
 
 
 def get_avg_temp(data, year=None, month=None) -> float:
-    pass
+    return 0.0
