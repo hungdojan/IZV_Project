7997f649226959fce027ad3be45a8306c4674c6a
Hung Do <hungdojan@gmail.com>
2022-10-24 19:26:38 +0200

    Initial commit

---
+++ Part1/part01.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+IZV cast1 projektu
+Autor: 
+
+Detailni zadani projektu je v samostatnem projektu e-learningu.
+Nezapomente na to, ze python soubory maji dane formatovani.
+
+Muzete pouzit libovolnou vestavenou knihovnu a knihovny predstavene na prednasce
+"""
+
+
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import List
+
+
+def integrate(x: np.array, y: np.array) -> float:
+    pass
+
+
+def generate_graph(a: List[float], show_figure: bool = False, save_path: str | None=None):
+    pass
+
+
+def generate_sinus(show_figure: bool=False, save_path: str | None=None):
+    pass
+
+
+def download_data(url="https://ehw.fit.vutbr.cz/izv/temp.html"):
+    pass
+
+
+def get_avg_temp(data, year=None, month=None) -> float:
+    pass
