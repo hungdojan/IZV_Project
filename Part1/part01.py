057ecbdd1023ccc63c7d36331991510075da2612
Hung Do <hungdojan@gmail.com>
2022-10-30 00:57:28 +0200

    Finish task01

--- Part1/part01.py
+++ Part1/part01.py
@@ -18,10 +18,28 @@ from typing import List
 
 
 def integrate(x: np.ndarray, y: np.ndarray) -> float:
+    """Returns result of integral of f(x) given array of X and Y values.
+
+    Args:
+        x (np.ndarray): X values.
+        y (np.ndarray): Mapped Y values.
+
+    Returns:
+        float: Resulted integral.
+    """
     return np.sum( (x[1:] - x[:-1]) * (y[:-1] + y[1:]) / 2)
 
 
 def generate_graph(a: List[float], show_figure: bool = False, save_path: str | None=None):
+    """Generates a simple graph of y = a * x**2.
+
+    Args:
+        a (List[float]): List of constants.
+        show_figure (bool, optional): Option to show generated figure.
+                                      Defaults to False.
+        save_path (str | None, optional): Save figure to file.
+                                          Defaults to None.
+    """
     # setup graph
     fig = plt.figure(figsize=(8, 4))
     ax = fig.add_subplot()
@@ -39,7 +57,6 @@ def generate_graph(a: List[float], show_figure: bool = False, save_path: str | N
         ax.annotate(xy=(x[-1], y[-1]), text=f'$\int f_{{{value}}}(x)dx$')
     ax.legend(loc='lower center', bbox_to_anchor=(0.5,1.0), ncol=len(a))
 
-
     if show_figure:
         plt.show()
     if save_path is not None:
@@ -49,6 +66,14 @@ def generate_graph(a: List[float], show_figure: bool = False, save_path: str | N
 
 
 def generate_sinus(show_figure: bool=False, save_path: str | None=None):
+    """Generate a complex sinus graph.
+
+    Args:
+        show_figure (bool, optional): Option to show generated figure.
+                                      Defaults to False.
+        save_path (str | None, optional): Save figure to file.
+                                          Defaults to None.
+    """
     # setup graph
     fig, ax = plt.subplots(3, 1, figsize=(6,4))
     for a in ax:
@@ -86,8 +111,47 @@ def generate_sinus(show_figure: bool=False, save_path: str | None=None):
 
 
 def download_data(url="https://ehw.fit.vutbr.cz/izv/temp.html") -> list:
-    return []
+    """Extract data from given HTML page.
+
+    Args:
+        url (str, optional): URL to HTML page. Defaults to "https://ehw.fit.vutbr.cz/izv/temp.html".
+
+    Returns:
+        list: List of extracted data.
+    """
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text)
+
+    lof_instances = []
+    table = soup.body.table.find_all('tr')
+    for row in table:
+        year_data = row.find_all('td')
+        year = year_data[0].p.text
+        month = year_data[1].p.text
+        temp = [float(x.p.text.replace(',', '.')) for x in year_data[2:]
+                if x.p is not None]
+        lof_instances.append({
+            "year": int(year),
+            "month": int(month),
+            "temp": np.array(temp)
+        })
+    return lof_instances
 
 
 def get_avg_temp(data, year=None, month=None) -> float:
-    return 0.0
+    """Return average temperature in specified time period.
+
+    Args:
+        data (dict[str, int|np.ndarray]): List of extracted data.
+        year (int, optional): Filter data by year. Defaults to None.
+        month (int, optional): Filter data by month. Defaults to None.
+
+    Returns:
+        float: Average temperature in specified time period.
+    """
+    filter_year = [x for x in data
+                   if year is None or x['year'] == year]
+    filter_month = [x['temp'] for x in filter_year
+                    if month is None or x['month'] == month]
+    filtered_data = np.concatenate(tuple(filter_month))
+    return np.average(filtered_data)
